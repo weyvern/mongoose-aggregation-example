7cc8bb8fe48d1d9a6d391cd4d2fa488596ef1b59
Jorge Paul <jserranopaul@gmail.com>
2021-04-29 17:57:19 +0200

    Location create and read

--- controllers/locations.js
+++ controllers/locations.js
@@ -3,7 +3,7 @@ import { Location } from '../models/Schemas.js';
 export const getAll = async (req, res) => {
   try {
     const locations = await Location.find();
-    res.status(201).json(locations);
+    res.status(200).json(locations);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -11,6 +11,9 @@ export const getAll = async (req, res) => {
 
 export const create = async (req, res) => {
   try {
+    const { name } = req.body;
+    const newLocation = await Location.create({ name });
+    res.status(201).json(newLocation);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -18,6 +21,9 @@ export const create = async (req, res) => {
 
 export const getSingle = async (req, res) => {
   try {
+    const { id } = req.params;
+    const location = await Location.findById(id);
+    res.status(200).json(location);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
